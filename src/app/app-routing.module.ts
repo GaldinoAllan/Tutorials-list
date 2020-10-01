cf1d045e913e104bfa2f487c976006c442fb8092
Allan Galdino <allan.g.galdino@outlook.com>
2020-10-01 18:29:45 -0300

    adding tutorials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailComponent } from './components/tutorial-detail/tutorial-detail.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: TutorialsListComponent },
+  { path: 'tutorials/:id', component: TutorialDetailComponent },
+  { path: 'add', component: AddTutorialComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Tutorial } from './tutorial';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/tutorials';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(this.baseUrl).pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  get(id): Observable<Tutorial> {
+    return this.http.get<Tutorial>(`${this.baseUrl}/${id}`);
+  }
+
+  create(data): Observable<Tutorial> {
+    return this.http.post<Tutorial>(this.baseUrl, data);
+  }
+
+  update(id, data): Observable<Tutorial> {
+    return this.http.put<Tutorial>(`${this.baseUrl}/${id}`, data);
+  }
+
+  delete(id): Observable<Tutorial> {
+    return this.http.delete<Tutorial>(`${this.baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<Tutorial> {
+    return this.http.delete<Tutorial>(this.baseUrl);
+  }
+
+  findByTitle(title): Observable<Tutorial> {
+    return this.http.get<Tutorial>(`${this.baseUrl}?title=${title}`);
+  }
+
+  errorHandler(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`
+    }
+    return throwError(errorMessage);
+  }
 }
